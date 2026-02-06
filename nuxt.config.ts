d7ae25e5e29d382e63666e83586682ca0dbde989
Cécile <cecilephan@outlook.com>
2026-02-06 16:02:01 +0100

    :test_tube: clean nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }, 
-    modules: ['@nuxtjs/device'],
+  modules: ['@nuxtjs/device'],
   css: ['~~/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss(), ]
